d41d047e07b7b759ebe3f848a1bdf43c6ed8fbac
Prajwal S <prajwalplays@gmail.com>
2024-01-30 23:40:07 +0530

    Create workoutModel.js

---
+++ backend/models/workoutModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose"); //importing mongoose library
+
+const Schema = mongoose.Schema; //creates a variable named Schema and assigns it the Schema property of the mongoose object
+
+const workoutSchema = new Schema(
+  {
+    //new schema called workoutSchema
+    title: {
+      type: String,
+      required: true,
+    },
+    weight: {
+      type: Number,
+      required: true,
+    },
+    reps: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+); //this automatically adds created fields
+
+module.exports = mongoose.model("workout", workoutSchema); //this exports the mongoose model based on the workoutSchema
