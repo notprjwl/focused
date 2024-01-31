5051126a516f34a6c8d39fb7655a5519cead1cf1
Prajwal S <prajwalplays@gmail.com>
2024-01-31 18:43:26 +0530

    Update workouts.js
    - removed the routes and replaced it with a function from the controller

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,39 +1,20 @@
-const express = require("express")
-const router = express.Router() //  the express.Router() function creates a new router object. A router in Express is a way to organize routes and their handlers in a separate module. 
-const Workout = require("../models/workoutModel")
+const express = require("express");
+const router = express.Router(); //  the express.Router() function creates a new router object. A router in Express is a way to organize routes and their handlers in a separate module.
+const { createWorkout, getWorkouts, getWorkout, deleteWorkout, updateWorkout } = require("../controllers/workoutController");
+
 // GET all workouts
-router.get('/', (req, res) => {
-    res.json({mesg:"GET all workouts"})
-})
+router.get("/", getWorkouts);
 
 // GET a single workouts
-router.get('/:id', (req, res) => {  // ': colen' represents route parameter. next to it can change
-    res.json({mesg:"GET a single workout"})
-})
+router.get("/:id", getWorkout);
 
 // POST a single workouts
-router.post('/', async (req, res)=> {
-    const {title, weight, reps} = req.body  // This is the property that holds the parsed request body. When a client sends data in the body of an HTTP request (for example, in a POST or PUT request), it needs to be parsed before it can be used. The body property is where this parsed data is stored.
-    
-    try{
-        const workout = await Workout.create({title, weight, reps})
-        res.status(200).json(workout)
-    }
-    catch(error){
-        res.status(400).json({error: error.message})
-    }
-})
+router.post("/", createWorkout);
 
 // DELETE a workout
-router.delete('/:id', (req, res)=> {
-    res.json({mesg:"DELETE a workout"})
-})
+router.delete("/:id", deleteWorkout);
 
 // UPDATE a workout
-router.patch('/:id', (req, res)=> {
-    res.json({mesg:"UPDATE a workout"})
-})
-
+router.patch("/:id", updateWorkout);
 
 module.exports = router; //export all the routes
-
