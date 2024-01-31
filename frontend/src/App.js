d161899018b4a16940acf827450598a02696bfb1
Prajwal S <prajwalplays@gmail.com>
2024-01-31 18:43:47 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' Component={Home} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
