e75f76d46e035284c73ad187c8115cde81e85971
Prajwal S <prajwalplays@gmail.com>
2024-02-07 16:25:26 +0530

    Create useWorkoutsContext.jsx
    - made a hook for the 'WorkoutsContext'

---
+++ frontend/src/hooks/useWorkoutsContext.jsx
@@ -0,0 +1,13 @@
+import React, { useContext } from "react";
+import { WorkoutsContext } from "../context/WorkoutsContext";
+
+const useWorkoutsContext = () => {
+  const context = useContext(WorkoutsContext);
+
+  if (!context) {
+    throw Error("useWorkoutContext must be used inside an WorkoutsContextProvider");
+  }
+  return context;
+};
+
+export { useWorkoutsContext };
