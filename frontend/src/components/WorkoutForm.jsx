4a88b2cc3cb63e6d4de6ad5e18d8485326aa7fa9
Prajwal S <prajwalplays@gmail.com>
2024-02-07 16:21:28 +0530

    Update WorkoutForm.jsx
    - added 'sets' properties
    - added 'useWorkoutContext' hook for easier access to the workout in the components
    - made changes regarding the global state by adding dispatch function to trigger the actions in the 'WorkoutContext' and update the global state
    - made a few ui changes

--- frontend/src/components/WorkoutForm.jsx
+++ frontend/src/components/WorkoutForm.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 // import useFetch from "../hooks/useFetch";
 
 const WorkoutForm = () => {
+  const { dispatch } = useWorkoutsContext();
   const [title, setTitle] = useState("");
   const [weight, setWeight] = useState("");
+  const [sets, setSets] = useState("");
   const [reps, setReps] = useState("");
   const [error, setError] = useState(null);
 
@@ -11,14 +14,14 @@ const WorkoutForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const workout = { title, weight, reps };
+    const workout = { title, weight, sets, reps };
     const resp = await fetch("/api/workouts", {
       method: "POST",
       body: JSON.stringify(workout),
       headers: {
         "Content-Type": "application/json",
       },
-    })
+    });
     const json = await resp.json();
     if (!resp.ok) {
       setError(json.error); //error prop in workoutcontroller
@@ -26,26 +29,35 @@ const WorkoutForm = () => {
     if (resp.ok) {
       setTitle("");
       setWeight("");
+      setSets("");
       setReps(""); // why because when you submit the content in the input field will get deleted
       setError(null);
       console.log("new workout added:", json);
+      dispatch({type: 'CREATE_WORKOUT', payload: json})
     }
   };
 
   return (
-    <form onSubmit={handleSubmit} className=' max-w-[200px] my-auto'>
-      <h3>Add a new workout!</h3>
-      <label>Exercise Title</label>
-      <input type='text' onChange={(e) => setTitle(e.target.value)} value={title} required className='border' />
-      <label>Weight(in kg):</label>
-      <input type='number' onChange={(e) => setWeight(e.target.value)} value={weight}  className='border' />
-      <label>Reps:</label>
-      <input type='number' onChange={(e) => setReps(e.target.value)} value={reps}  className='border' />
-      <button className='text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800'>
-        Submit
-      </button>
-      {error && <div className=''>{error}</div>}
-    </form>
+    <div className='border-2 p-5 rounded-xl'>
+      <form onSubmit={handleSubmit} className='max-w-[200px] my-auto font-poppins'>
+        <h3 className='text-center font-medium pb-3'>Add a new workout!</h3>
+        <label className=''>Exercise Title:</label>
+        <div className='items-center justify-center'>
+          <input type='text' onChange={(e) => setTitle(e.target.value)} value={title} required className='border' />
+        </div>
+        <label>Weight(in kg):</label>
+        <input type='number' onChange={(e) => setWeight(e.target.value)} value={weight} className='border' />
+        <label>Sets:</label>
+        <input type='number' onChange={(e) => setSets(e.target.value)} value={sets} className='border' />
+        <label>Reps:</label>
+        <input type='number' onChange={(e) => setReps(e.target.value)} value={reps} className='border' />
+        <div className='text-center'>
+          <button className='bg-[#F8F4F9] font-poppins shadow-md text-red p-2 mt-5 rounded-md text-sm font-bold hover:bg-red hover:text-[#F8F4F9]'>Submit</button>
+        </div>
+
+        {error && <div className=''>{error}</div>}
+      </form>
+    </div>
   );
 };
 
