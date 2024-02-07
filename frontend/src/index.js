e0397ece71051a38f3deea4f39781291658d577a
Prajwal S <prajwalplays@gmail.com>
2024-02-07 16:26:27 +0530

    Update index.js
    - added 'WorkoutContextProvider' that  wraps the whole app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { WorkoutContextProvider } from "./context/WorkoutsContext";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <WorkoutContextProvider>
+      <App />
+    </WorkoutContextProvider>
   </React.StrictMode>
 );
 
