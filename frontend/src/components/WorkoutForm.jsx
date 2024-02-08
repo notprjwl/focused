bfb4195d16cdea0befc72651ab87d8692b6051d9
Prajwal S <prajwalplays@gmail.com>
2024-02-08 21:04:53 +0530

    Update WorkoutForm.jsx

--- frontend/src/components/WorkoutForm.jsx
+++ frontend/src/components/WorkoutForm.jsx
@@ -33,28 +33,33 @@ const WorkoutForm = () => {
       setReps(""); // why because when you submit the content in the input field will get deleted
       setError(null);
       console.log("new workout added:", json);
-      dispatch({type: 'CREATE_WORKOUT', payload: json})
+      dispatch({ type: "CREATE_WORKOUT", payload: json });
     }
   };
 
   return (
-    <div className='border-2 p-5 rounded-xl'>
-      <form onSubmit={handleSubmit} className='max-w-[200px] my-auto font-poppins'>
-        <h3 className='text-center font-medium pb-3'>Add a new workout!</h3>
+    <div className='w-[80%] mt-4 border-2 hover:border-text p-5 rounded-xl max-w-[250px]'>
+      <form onSubmit={handleSubmit} className=' my-auto font-poppins'>
+        <h3 className='text-center text-3xl font-honk font-medium pb-3'>Add a new workout!</h3>
         <label className=''>Exercise Title:</label>
-        <div className='items-center justify-center'>
-          <input type='text' onChange={(e) => setTitle(e.target.value)} value={title} required className='border' />
+        <div className='flex items-center justify-center pb-3'>
+          <input type='text' onChange={(e) => setTitle(e.target.value)} value={title} required className='border hover:border-teal-300 border-teal-400' />
         </div>
         <label>Weight(in kg):</label>
-        <input type='number' onChange={(e) => setWeight(e.target.value)} value={weight} className='border' />
+        <div className='flex items-center justify-center pb-3'>
+          <input type='number' onChange={(e) => setWeight(e.target.value)} value={weight} className='border hover:border-teal-300 border-teal-400' />
+        </div>
         <label>Sets:</label>
-        <input type='number' onChange={(e) => setSets(e.target.value)} value={sets} className='border' />
+        <div className='flex items-center justify-center pb-3'>
+          <input type='number' onChange={(e) => setSets(e.target.value)} value={sets} className='border hover:border-teal-300 border-teal-400' />
+        </div>
         <label>Reps:</label>
-        <input type='number' onChange={(e) => setReps(e.target.value)} value={reps} className='border' />
+        <div className='flex items-center justify-center'>
+          <input type='number' onChange={(e) => setReps(e.target.value)} value={reps} className='border hover:border-teal-300 border-teal-400' />
+        </div>
         <div className='text-center'>
           <button className='bg-[#F8F4F9] font-poppins shadow-md text-red p-2 mt-5 rounded-md text-sm font-bold hover:bg-red hover:text-[#F8F4F9]'>Submit</button>
         </div>
-
         {error && <div className=''>{error}</div>}
       </form>
     </div>
