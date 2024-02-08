245d382a24493509df6be5a3d4b02c4f1f2bd3e8
Prajwal S <prajwalplays@gmail.com>
2024-02-08 21:11:48 +0530

    Update useWorkoutsContext.jsx

--- frontend/src/hooks/useWorkoutsContext.jsx
+++ frontend/src/hooks/useWorkoutsContext.jsx
@@ -3,7 +3,7 @@ import { WorkoutsContext } from "../context/WorkoutsContext";
 
 const useWorkoutsContext = () => {
   const context = useContext(WorkoutsContext);
-
+  
   if (!context) {
     throw Error("useWorkoutContext must be used inside an WorkoutsContextProvider");
   }
