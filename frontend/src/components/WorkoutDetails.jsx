a16bf8a2df24454388d2e9fa0b938039c51bfa2f
Prajwal S <prajwalplays@gmail.com>
2024-02-15 20:36:17 +0530

    Update WorkoutDetails.jsx
    - added table to represent the workouts
    - made a few changes to the ui

--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 import UpdateModal from "./UpdateModal";
+import { Funnel } from "@heroicons/react/outline";
+import useFetch from "../hooks/useFetch";
 
-const WorkoutDetails = ({ workout }) => {
-  const newCreatedAt = workout.createdAt.slice(0, 10);
+const WorkoutDetails = () => {
+  const { workouts: workout, loading, error } = useFetch("/api/workouts");
+
+  // const newCreatedAt = workout.createdAt.slice(0, 10);
   const { dispatch } = useWorkoutsContext();
 
   const [modal, setModal] = useState(false);
@@ -15,7 +19,6 @@ const WorkoutDetails = ({ workout }) => {
     }, 100);
     return () => clearTimeout(timeOut);
   }, [setTransition]);
-
 
   const handleDelete = async () => {
     const response = await fetch("api/workouts/" + workout._id, {
@@ -36,47 +39,106 @@ const WorkoutDetails = ({ workout }) => {
   };
 
   return (
-    <div className='text-back font-poppins p-6 shadow-lg mb-9 max-w-[1400px] rounded-lg '>
-      <h4 className='font-bold text-red mb-3'>{workout.title}</h4>
-      <p>
-        <strong>Weight(kg): </strong>
-        {workout.weight}
-      </p>
-      <p>
-        <strong>Sets: </strong>
-        {workout.sets}
-      </p>
-      <p>
-        <strong>Reps: </strong>
-        {workout.reps}
-      </p>
-      <p>
-        <strong>Created At: </strong>
-        {newCreatedAt}
-      </p>
-      <span className='flex justify-end'>
-        <button className='absolute mt-[-130px] bg-[#F8F4F9] font-poppins shadow-md text-red p-2 rounded-md text-sm font-bold hover:bg-red hover:text-[#F8F4F9] transition-all duration-500 ease-in-out' onClick={ismodalOpen}>
-          Update
-        </button>
-      </span>
-      <span className='flex justify-end'>
-        <button className='absolute mt-[-40px] bg-[#F8F4F9] font-poppins shadow-md text-red p-2 rounded-md text-sm font-bold hover:bg-red hover:text-[#F8F4F9]' onClick={handleDelete}>
-          Delete
-        </button>
-      </span>
-      {modal && (
-        <UpdateModal
-          closeModal={isModalClose}
-          initialValues={{
-            title: workout.title,
-            weight: workout.weight,
-            sets: workout.sets,
-            reps: workout.reps,
-            _id: workout._id
-          }}
-        />
-      )}
-    </div>
+    // <div className='text-gray-300 font-poppins p-6 shadow-lg mb-9 rounded-lg '>
+    //   <h4 className='font-bold text-gray-300 mb-3'>{workout.title}</h4>
+    //   <p>
+    //     <strong>Weight(kg): </strong>
+    //     {workout.weight}
+    //   </p>
+    //   <p>
+    //     <strong>Sets: </strong>
+    //     {workout.sets}
+    //   </p>
+    //   <p>
+    //     <strong>Reps: </strong>
+    //     {workout.reps}
+    //   </p>
+    //   <p>
+    //     <strong>Created At: </strong>
+    //     {newCreatedAt}
+    //   </p>
+    //   {/* <span className='flex justify-end'>
+    //     <button className='absolute mt-[-130px] bg-[#F8F4F9] font-poppins shadow-md text-red p-2 rounded-md text-sm font-bold hover:bg-red hover:text-[#F8F4F9] transition-all duration-500 ease-in-out' onClick={ismodalOpen}>
+    //       Update
+    //     </button>
+    //   </span>
+    //   <span className='flex justify-end'>
+    //     <button className='absolute mt-[-40px] bg-[#F8F4F9] font-poppins shadow-md text-red p-2 rounded-md text-sm font-bold hover:bg-red hover:text-[#F8F4F9]' onClick={handleDelete}>
+    //       Delete
+    //     </button>
+    //   </span> */}
+    //   {modal && (
+    //     <UpdateModal
+    //       closeModal={isModalClose}
+    //       initialValues={{
+    //         title: workout.title,
+    //         weight: workout.weight,
+    //         sets: workout.sets,
+    //         reps: workout.reps,
+    //         _id: workout._id
+    //       }}
+    //     />
+    //   )}
+    // </div>
+    <body className='p-2'>
+      <div className='text-textTint bg-background pt-2 sm:pt-5 mx-auto rounded-3xl border-[1px] border-text'>
+        <div className='items-center overflow-x-hidden'>
+          <div className='px-10 py-2 flex justify-between items-center text-center'>
+            <label className='input input-bordered flex items-center w-[25%] h-[40px]'>
+              <input type='text' className='grow' placeholder='Search' />
+              <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='currentColor' className='w-4 h-4 opacity-70'>
+                <path fillRule='evenodd' d='M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z' clipRule='evenodd' />
+              </svg>
+            </label>
+            <h1 className='justify-center sm:text-sm md:text-sm lg:text-lg font-bold xxl:mr-[10px] w-[25%]'>EXERCISE LOG</h1>
+            <div className='flex justify-end  w-[25%]'>
+            <div className='dropdown dropdown-bottom dropdown-end h-10 justify-center pb-2'>
+              <div tabIndex={0} role='button' className='btn m-1 bg-transparent mb-2'>
+                <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-6 h-6'>
+                  <path strokeLinecap='round' strokeLinejoin='round' d='M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z' />
+                </svg>
+              </div>
+              <ul tabIndex={0} className='dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-[7rem]'>
+                <li>
+                  <a>By Date</a>
+                </li>
+                <li>
+                  <a>By Reps</a>
+                </li>
+              </ul>
+            </div>
+            </div>
+          </div>
+          <table className='table-auto mx-auto sm:w-full md:w-full lg:w-[100%] xl:w-[100%]'>
+            <thead className='border-b-[1px] border-text'>
+              <tr className='justify-start text-left'>
+                <th></th>
+                <th className='px-2 py-2 sm:px-3 w-[30%] text-lg sm:text-sm md:text-2xl '>Exercise Name</th>
+                <th className='px-2 py-2 sm:px-3 w-[10%] text-lg sm:text-sm md:text-2xl'>Weight</th>
+                <th className='px-2 py-2 sm:px-3 w-[10%] text-lg sm:text-sm md:text-2xl'>Sets</th>
+                <th className='px-2 py-2 sm:px-3 w-[5%] text-lg sm:text-sm md:text-2xl'>Reps</th>
+              </tr>
+            </thead>
+            <tbody className=''>
+              {workout &&
+                workout.map((workout) => (
+                  <tr key={workout._id} className='text-left border-t-[1px] border-text'>
+                    <td className='w-[1%] pl-3'>
+                      <input key={workout._id} type='checkbox' className='checkbox text-center align-middle' />
+                    </td>
+                    <td className='px-2 py-2 sm:px-3 text-sm sm:text-base md:text-lg pt-2'>{workout.title}</td>
+                    <td className='px-2 py-2  sm:px-3 text-sm sm:text-base md:text-lg'>{workout.weight}</td>
+                    <td className='px-2 py-2  sm:px-3 text-sm sm:text-base md:text-lg'>{workout.sets}</td>
+                    <td className='px-2 py-2  sm:px-3 text-sm sm:text-base md:text-lg'>{workout.reps}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+          <div className=''></div>
+        </div>
+        <div className=' bg-black rounded-b-3xl px-3 '>hey</div>
+      </div>
+    </body>
   );
 };
 
