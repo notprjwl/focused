adb46117796cf043c5d17d1763c1f885c2e4807b
Prajwal S <prajwalplays@gmail.com>
2024-02-15 20:36:22 +0530

    Update Main.jsx

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -28,13 +28,12 @@ const Main = () => {
       )}
       {!loading && (
         <div className='grid place-items-center'>
-          <button className="btn btm-sm bg-formBg text-gray-300" onClick={modalOpen}>Add an Exercise</button>
+          <button className="btn btm-sm bg-[#19191aa8] text-gray-300 hover:bg-green-800 hover:text-white hover:border border-[#3a645d86] " onClick={modalOpen}>Add an Exercise</button>
         </div>
       )}
       {modal && (<WorkoutForm closeModal={modalClose}/>)}
-      <div className='text-text p-5 max-w-[1200px] items-center justify-between mx-auto'>
-        <div className=''>{workouts && workouts.map((workout) => <WorkoutDetails key={workout._id} workout={workout} />)}</div>
-
+      <div className='max-w-[1000px] mx-auto mt-10'>
+        {<WorkoutDetails />}
         {/* <button type='button' className='text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500 float-right'>
             <svg className='w-4 h-4' aria-hidden='true' xmlns='http://www.w3.org/2000/svg' fill='currentColor' viewBox='0 0 18 18'>
               <path d='M3 7H1a1 1 0 0 0-1 1v8a2 2 0 0 0 4 0V8a1 1 0 0 0-1-1Zm12.954 0H12l1.558-4.5a1.778 1.778 0 0 0-3.331-1.06A24.859 24.859 0 0 1 6 6.8v9.586h.114C8.223 16.969 11.015 18 13.6 18c1.4 0 1.592-.526 1.88-1.317l2.354-7A2 2 0 0 0 15.954 7Z' />
