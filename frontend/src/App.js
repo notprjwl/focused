9f0e7619ad21dcd19fded7f5fa7dd9e6021c8d5d
Prajwal S <prajwalplays@gmail.com>
2024-02-21 22:16:22 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 
+
 function App() {
   return (
     <div className='App'>
@@ -15,6 +16,7 @@ function App() {
           <Route path='/workouts' Component={Main} />
           <Route path='/login' Component={Login} />
           <Route path='/signup' Component={SignUp} />
+          
         </Routes>
       </BrowserRouter>
     </div>
