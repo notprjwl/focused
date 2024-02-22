641d988920054a6da56ac4e610a0e5addf7bf8e0
Prajwal S <prajwalplays@gmail.com>
2024-02-22 22:15:20 +0530

    Create userModel.js
    - created schema for user.
    - added few error checks for username, email and password in signup.
    - added module bcrypt for password hashing, and validator for input validation.
    -validates that all fields are filled, checks if the email is valid, and ensures the password is strong.
    -queries the database to check if the provided username or email already exists, throwing errors if they do.
    -uses bcrypt to generate a salt and hash for the password.

---
+++ backend/models/userModel.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+
+const schema = mongoose.Schema;
+
+const userSchema = new schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// static signup method for usermodel //signup fucntion is created here. you can use it anywhere just call the function.
+userSchema.statics.signup = async function (username, email, password) {
+  if (!username || !email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  if (!validator.isEmail(email)) {
+    throw Error("Email is not valid");
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password not strong enough");
+  }
+
+  const emailExists = await this.findOne({ email }); //we can use User instead of this but it is going to export at the end. that is why we are using this to access userModel
+  const usernameExists = await this.findOne({ username });
+
+  if (usernameExists) {
+    throw Error("username already in use");
+  }
+  if (emailExists) {
+    throw Error("mail already in use");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ username, email, password: hash });
+
+  return user;
+};
+
+module.exports = mongoose.model("User", userSchema);
