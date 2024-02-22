6e420a3685efdb1d4df43135aa1d43c03cbbde86
Prajwal S <prajwalplays@gmail.com>
2024-02-22 22:11:37 +0530

    Create userController.js
    - created a new controller for user login/signup

---
+++ backend/controllers/userController.js
@@ -0,0 +1,20 @@
+const User = require("../models/userModel");
+
+// login user
+const loginUser = async (req, res) => {
+  res.json({ message: "login user" });
+};
+
+// signup user
+const signupUser = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const user = await User.signup(username, email, password);
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = { loginUser, signupUser };
