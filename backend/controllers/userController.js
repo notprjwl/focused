3be9c68ec403db342fafbb0c940d61ffce753e2f
Prajwal S <prajwalplays@gmail.com>
2024-02-26 21:50:32 +0530

    Update userController.js
    - made changes to loginUser function to process login requests.
    - extracted username, email, password from the request body.
    - calls the user.login method with the provided credentials.
    - If it is successful, generates a token using the createToken function and sends a response with the user's email and the generated token.
    - If it is unsuccessful, catches errors thrown during the login process and sends a response with the error message.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,7 +8,17 @@ const createToken = (_id) => {
 
 // login user
 const loginUser = async (req, res) => {
-  res.json({ message: "login user" });
+  const { username, email, password } = req.body;
+
+  try {
+    const user = await User.login(username, email, password);
+
+    //creating a token
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 // signup user
