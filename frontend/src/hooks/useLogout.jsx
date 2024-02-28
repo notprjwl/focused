c4ea1389a0b92ed8108e17532570061b5aaaf948
Prajwal S <prajwalplays@gmail.com>
2024-02-28 21:04:00 +0530

    Create useLogout.jsx
    - created logout hook, and implemented the functionality.

---
+++ frontend/src/hooks/useLogout.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useAuthContext } from "./useAuthContext";
+import { useSignup } from "./useSignup";
+
+const useLogout = () => {
+  const { dispatch } = useAuthContext();
+  const { accessToken, setAccessToken } = useSignup();
+  const logout = () => {
+    // // remove user from storage
+    // localStorage.removeItem("user");
+    setAccessToken(null);
+    // dispatch logout action
+    dispatch({ type: "LOGOUT" });
+    // remove the refresh token from the httponly cookie
+    document.cookie = "refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=;";
+  };
+  // Forcefully update the application log
+  setTimeout(() => {
+    console.log("Application log after logout:", document.cookie);
+  }, 0);
+
+  return { logout };
+};
+
+export default useLogout;
