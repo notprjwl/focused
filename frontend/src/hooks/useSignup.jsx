9b366ac87a473360858b0ce62968c3b2443458a8
Prajwal S <prajwalplays@gmail.com>
2024-02-28 21:06:26 +0530

    Update useSignup.jsx
    - trying to remove the localStorage by adding cookies. (might change cookie to another cause cookie also is not safe)

--- frontend/src/hooks/useSignup.jsx
+++ frontend/src/hooks/useSignup.jsx
@@ -6,7 +6,8 @@ const useSignup = () => {
   const [error, setError] = useState(null);
   const [isloading, setIsLoading] = useState(null);
   const { dispatch } = useAuthContext();
-  const [emptyFields, setEmptyFields] = useState([])
+  const [emptyFields, setEmptyFields] = useState([]);
+  const [accessToken, setAccessToken] = useState(null);
 
   const signup = async (username, email, password) => {
     setIsLoading(true);
@@ -20,21 +21,25 @@ const useSignup = () => {
     });
 
     const json = await response.json();
+    console.log("json response: ", json);
     if (!response.ok) {
       setIsLoading(false);
       setError(json.error);
       setEmptyFields(json.emptyFields);
     }
     if (response.ok) {
-      // saving the user to local storage
-      localStorage.setItem("user", JSON.stringify(json));
+      // // saving the user to local storage
+      // localStorage.setItem("user", JSON.stringify(json));
+      const { token, refreshToken } = json;
+      setAccessToken(token);
+      document.cookie = `refreshToken=${refreshToken}; path=/; samesite=strict; httponly`;
       dispatch({ type: "LOGIN", payload: json });
       setIsLoading(false);
       setEmptyFields([]);
     }
   };
 
-  return { signup, error, isloading, emptyFields };
+  return { signup, error, isloading, emptyFields, accessToken, setAccessToken };
 };
 
 export { useSignup };
