09e72bd8b1b3d14f8176322436c9b8076b3a6bcc
Prajwal S <prajwalplays@gmail.com>
2024-02-29 20:55:03 +0530

    Create useLogin.jsx
    - made a new useLogin hook to login.
    - it is very similar to useSignup hook but here it basically takes two arguments.

---
+++ frontend/src/hooks/useLogin.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { useAuthContext } from "./useAuthContext";
+import Login from "../pages/Login";
+
+const useLogin = () => {
+  const [error, setError] = useState(null);
+  const [isloading, setIsLoading] = useState(null);
+  const { dispatch } = useAuthContext();
+  const [emptyFields, setEmptyFields] = useState([]);
+  const [accessToken, setAccessToken] = useState(null);
+
+  const login = async (usernameOrEmail, password) => {
+    setIsLoading(true);
+    setError(null);
+    const response = await fetch("/api/user/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ usernameOrEmail, password }),
+    });
+
+    const json = await response.json();
+    console.log("json response: ", json);
+    if (!response.ok) {
+      setIsLoading(false);
+      setError(json.error);
+      setEmptyFields(json.emptyFields);
+    }
+    if (response.ok) {
+      // // saving the user to local storage
+      // localStorage.setItem("user", JSON.stringify(json));
+      const { token, refreshToken } = json;
+      setAccessToken(token);
+      document.cookie = `refreshToken=${refreshToken}; path=/; samesite=strict; httponly`;
+      dispatch({ type: "LOGIN", payload: json });
+      setIsLoading(false);
+      setEmptyFields([]);
+    }
+  };
+
+  return { login, error, isloading, emptyFields, accessToken, setAccessToken };
+};
+
+export { useLogin };
