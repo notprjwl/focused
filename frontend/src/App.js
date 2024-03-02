8f3ef5dbbdbab67fc9df67c783778d4842343678
Prajwal S <prajwalplays@gmail.com>
2024-03-02 21:26:50 +0530

    Update App.js
    - protected routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Main from "./pages/Main";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
-
+import { useAuthContext } from "./hooks/useAuthContext";
 
 function App() {
+  const { user } = useAuthContext();
   return (
     <div className='App'>
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path='/' Component={Home} />
-          <Route path='/workouts' Component={Main} />
-          <Route path='/login' Component={Login} />
-          <Route path='/signup' Component={SignUp} /> 
+          <Route path='/' element={!user ? <Home /> : <Navigate to="/workouts"/>} />
+          <Route path='/workouts' element={user ? <Main /> : <Navigate to="/login" />} />
+          <Route path='/login' element={!user ? <Login /> : <Navigate to ="/workouts" />} />
+          <Route path='/signup' element={!user ? <SignUp/> : <Navigate to="/login" />} />
+          <Route path='*' element={<Navigate to='/' />} />
         </Routes>
       </BrowserRouter>
     </div>
