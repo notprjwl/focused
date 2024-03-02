7f0567b3743a793e30366dcd13b7fb27dc6bae8c
Prajwal S <prajwalplays@gmail.com>
2024-03-02 21:27:03 +0530

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,27 +1,36 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, useEffect } from "react";
 
-const AuthContext = createContext();	// creating context
+const AuthContext = createContext(); // creating context
 
-const authReducer = (state, action) => {	
+const authReducer = (state, action) => {
   switch (action.type) {
-    case "LOGIN":			
-      return { user: action.payload };
+    case "LOGIN":
+      return { user: action.payload,  actionType: "LOGIN" };
+
+    case "SIGNUP":
+      return { user: action.payload, actionType: "SIGNUP"};
 
     case "LOGOUT":
-      return { user: null };
+      return { user: null, actionType: null };
 
     default:
       return state;
   }
 };
 
+// 
+const user = JSON.parse(localStorage.getItem('user')) || null
+
 const AuthContextProvider = ({ children }) => {
+
   const [state, dispatch] = useReducer(authReducer, {
-    user: null,	// initially state is null
+    user: user, // initially state is null
   });
+
+
   console.log("Auth context state: ", state);
 
   return <AuthContext.Provider value={{ ...state, dispatch }}>{children}</AuthContext.Provider>;
 };
 
-export {AuthContext, authReducer, AuthContextProvider}
+export { AuthContext, authReducer, AuthContextProvider };
