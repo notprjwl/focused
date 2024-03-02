0390dd3c4b22d9a4121448bf28cd95cf9caadea5
Prajwal S <prajwalplays@gmail.com>
2024-03-02 21:27:13 +0530

    Update useLogout.jsx

--- frontend/src/hooks/useLogout.jsx
+++ frontend/src/hooks/useLogout.jsx
@@ -2,25 +2,21 @@ import React from "react";
 import { useAuthContext } from "./useAuthContext";
 import { useSignup } from "./useSignup";
 
-const useLogout = (req, res) => {
+const useLogout = () => {
   const { dispatch } = useAuthContext();
   const { setAccessToken } = useSignup();
   const logout = () => {
-    console.log("Application log before logout:", document.cookie);
-
-    // // remove user from storage
-    // localStorage.removeItem("user");
+    // remove user from storage
+    localStorage.removeItem("user");
+    
     setAccessToken(null);
+
     // dispatch logout action
     dispatch({ type: "LOGOUT" });
     // remove the refresh token from the httponly cookie
-    document.cookie = `refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=`;
-
+    // Deleting access_token cookie with implicit domain.
+    // document.cookie = "refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
   };
-  // Forcefully update the application log
-  setTimeout(() => {
-    console.log("Application log after logout:", document.cookie);
-  }, 0);
 
   return { logout };
 };
