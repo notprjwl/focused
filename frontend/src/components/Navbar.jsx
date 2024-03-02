a7f289de42b01ffac09ba9c32863e572dcd289f9
Prajwal S <prajwalplays@gmail.com>
2024-03-02 21:26:53 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import Main from "../pages/Main";
 import useLogout from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
 
   const handleLogout = () => {
-    logout()
+    logout();
   };
 
   return (
@@ -29,14 +31,21 @@ const Navbar = () => {
           <Link className='pr-2'>
             <h1>Charts</h1>
           </Link> */}
-          <Link to='/workouts' className='animate-none hover:animate-spin transition ease-in-out'>
-            <svg className='h-6 w-6 text-text' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='1.4' strokeLinecap='round' strokeLinejoin='round'>
-              <circle cx='12' cy='12' r='10' /> <line x1='12' y1='8' x2='12' y2='16' /> <line x1='8' y1='12' x2='16' y2='12' />
-            </svg>
-          </Link>
-          <div className='mx-5'>
-            <button onClick={handleLogout}>logout</button>
+          
+            <Link to='/workouts' className='animate-none hover:animate-spin transition ease-in-out'>
+              <svg className='h-6 w-6 text-text' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='1.4' strokeLinecap='round' strokeLinejoin='round'>
+                <circle cx='12' cy='12' r='10' /> <line x1='12' y1='8' x2='12' y2='16' /> <line x1='8' y1='12' x2='16' y2='12' />
+              </svg>
+            </Link>
+          
+          {user && (
+            <div className="dropdown dropdown-end">
+            <div tabIndex={0} role="button" className="mr-3">{user.usernameOrEmail}</div>
+            <ul tabIndex={0} className="dropdown-content z-[1] menu shadow bg-formBg mt-2 rounded-md w-25 hover:bg-black">
+              <li onClick={handleLogout} className=""><a className="p-1 rounded-sm hover:bg-black">Logout</a></li>
+            </ul>
           </div>
+          )}
         </div>
       </div>
     </header>
