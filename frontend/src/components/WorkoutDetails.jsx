7dded5a9120dbffb81b56ac837bb765cb5910a4f
Prajwal S <prajwalplays@gmail.com>
2024-03-06 17:13:15 +0530

    Update WorkoutDetails.jsx

--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -100,50 +100,50 @@ const WorkoutDetails = () => {
   // }, [sortOrder, workout]);
 
   useEffect(() => {
-    if(initialWorkout){
-    const prevSortOrder = { ...sortOrder };
-    const sortedWorkout = [...initialWorkout].sort((a, b) => {
-      let comparison = 0;
+    if (initialWorkout) {
+      const prevSortOrder = { ...sortOrder };
+      const sortedWorkout = [...initialWorkout].sort((a, b) => {
+        let comparison = 0;
 
-      Object.keys(sortOrder).some((columnName) => {
-        let columnA, columnB;
+        Object.keys(sortOrder).some((columnName) => {
+          let columnA, columnB;
 
-        if (columnName === "weight" || columnName === "sets" || columnName === "reps") {
-          columnA = parseFloat(a[columnName]);
-          columnB = parseFloat(b[columnName]);
-        } else if (columnName === "title") {
-          // Handle numeric values in 'Exercise Name' column
-          columnA = a[columnName];
-          columnB = b[columnName];
-          const numericA = parseFloat(columnA);
-          const numericB = parseFloat(columnB);
+          if (columnName === "weight" || columnName === "sets" || columnName === "reps") {
+            columnA = parseFloat(a[columnName]);
+            columnB = parseFloat(b[columnName]);
+          } else if (columnName === "title") {
+            // Handle numeric values in 'Exercise Name' column
+            columnA = a[columnName];
+            columnB = b[columnName];
+            const numericA = parseFloat(columnA);
+            const numericB = parseFloat(columnB);
 
-          if (!isNaN(numericA) && !isNaN(numericB)) {
-            columnA = numericA;
-            columnB = numericB;
+            if (!isNaN(numericA) && !isNaN(numericB)) {
+              columnA = numericA;
+              columnB = numericB;
+            }
+          } else {
+            columnA = a[columnName] ? a[columnName].toString().toLowerCase() : "";
+            columnB = b[columnName] ? b[columnName].toString().toLowerCase() : "";
           }
-        } else {
-          columnA = a[columnName] ? a[columnName].toString().toLowerCase() : "";
-          columnB = b[columnName] ? b[columnName].toString().toLowerCase() : "";
-        }
 
-        if (columnA < columnB) {
-          comparison = -1;
-        } else if (columnA > columnB) {
-          comparison = 1;
-        }
+          if (columnA < columnB) {
+            comparison = -1;
+          } else if (columnA > columnB) {
+            comparison = 1;
+          }
 
-        if (sortOrder[columnName] === "desc") {
-          comparison *= -1;
-        }
+          if (sortOrder[columnName] === "desc") {
+            comparison *= -1;
+          }
 
-        return comparison !== 0;
+          return comparison !== 0;
+        });
+
+        return comparison;
       });
-
-      return comparison;
-    });
-    setWorkout(sortedWorkout);
-  }
+      setWorkout(sortedWorkout);
+    }
   }, [sortOrder, initialWorkout]);
 
   useEffect(() => {
@@ -413,7 +413,14 @@ const WorkoutDetails = () => {
               ) : (
                 <tr>
                   <td colSpan='5' className='text-center py-4'>
-                    No data available
+                    {loading && (
+                      <div className='grid place-items-center'>
+                        <svg aria-hidden='true' className='inline w-8 h-[90vh] text-background animate-spin fill-text' viewBox='0 0 100 101' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                          <path d='M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z' fill='currentColor' />
+                          <path d='M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z' fill='currentFill' />
+                        </svg>
+                      </div>
+                    )}
                   </td>
                 </tr>
               )}
