b132f678a1181f0efa3be5504e8548afb738660b
Prajwal S <prajwalplays@gmail.com>
2024-03-07 16:23:36 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
 import { useAuthContext } from "./hooks/useAuthContext";
 
 function App() {
@@ -13,10 +14,11 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path='/' element={!user ? <Home /> : <Navigate to="/workouts"/>} />
-          <Route path='/workouts' element={user ? <Main /> : <Navigate to="/login" />} />
-          <Route path='/login' element={!user ? <Login /> : <Navigate to ="/workouts" />} />
-          <Route path='/signup' element={!user ? <SignUp/> : <Navigate to="/login" />} />
+          <Route path='/' element={!user ? <Home /> : <Navigate to='/dashboard' />} />
+          <Route path='/dashboard' element={user ? <Dashboard /> : <Navigate to='/' />} />
+          <Route path='/workouts' element={user ? <Main /> : <Navigate to='/login' />} />
+          <Route path='/login' element={!user ? <Login /> : <Navigate to='/dashboard' />} />
+          <Route path='/signup' element={!user ? <SignUp /> : <Navigate to='/login' />} />
           <Route path='*' element={<Navigate to='/' />} />
         </Routes>
       </BrowserRouter>
